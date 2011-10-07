4866c41c13f02608299179aa3775ab027f4266db
= <=>
2011-10-07 14:00:38 -0700

    initial add

---
+++ NaoKinectTest/HeadPanning.cs
@@ -0,0 +1,85 @@
+﻿/**
+ * @author Aldebaran Robotics
+ * Aldebaran Robotics (c) 2009 All Rights Reserved.\n
+ *
+ * Version : $Id$
+ */
+using System;
+using System.Collections;
+
+using Aldebaran.Proxies;
+
+namespace Aldebaran.NaoCamCSharpExample
+{
+    class HeadPanning
+    {
+        public MotionProxy _motion = null;
+
+        private float _minYaw = 0.0f;
+        private float _maxYaw = 0.0f;
+        private float _minPitch = 0.0f;
+        private float _maxPitch = 0.0f;
+
+        public void Connect(string ip)
+        {
+            try
+            {
+                _motion = new MotionProxy(ip,9559);
+                // --------------- prepare limits --------------------------
+                // three floats as an ArrayList for each joint in the chain
+                // min,max,maxNoLoadSpeedPerCycle
+                ArrayList limits = (ArrayList)_motion.getLimits("Head");
+                ArrayList yawLimits = (ArrayList)limits[0];
+                ArrayList pitchLimits = (ArrayList)limits[1];
+                _minYaw = (float)yawLimits[0];
+                _maxYaw = (float)yawLimits[1];
+                _minPitch = (float)pitchLimits[0];
+                _maxPitch = (float)pitchLimits[1];
+                Console.WriteLine("MinYaw: " + _minYaw + " MaxYaw:" + _maxYaw + " MinPitch: " + _minPitch + " MaxPitch:" + _maxPitch);
+                // give the joints some stiffness
+                _motion.stiffnessInterpolation("Head",1.0f,1.0f);
+
+            } 
+            catch(Exception e)
+            {
+                Console.Out.WriteLine("HeadPanning.Connect exception: " + e);
+            }
+        }
+
+        public void UpdateYaw(int val)
+        {
+            if (_motion != null)
+            {
+                try
+                {
+                    _motion.setAngles("HeadYaw", ScaleToRange(-val, _minYaw, _maxYaw),0.1f);
+                }
+                catch (Exception e)
+                {
+                    Console.Out.WriteLine("HeadPanning.UpdateYaw exception: " + e);
+                }
+            }
+        }
+
+        public void UpdatePitch(int val)
+        {
+            if (_motion != null)
+            {
+                try
+                {
+                    _motion.setAngles("HeadPitch", ScaleToRange(-val, _minPitch, _maxPitch),0.1f);
+                }
+                catch (Exception e)
+                {
+                    Console.Out.WriteLine("HeadPanning.UpdateYaw exception: " + e);
+                }
+            }
+        }
+
+        private static float ScaleToRange(int val, float min, float max)
+        {
+            float returnVal = (((val + 100.0f)/200.0f)*(max - min)) + min;
+            return returnVal;
+        }
+    }
+}
---
+++ NaoKinectTest/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Aldebaran.NaoCamCSharpExample;
+using Coding4Fun.Kinect.Wpf;
+using Microsoft.Research.Kinect.Nui;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace NaoKinectTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Runtime nui = new Runtime();
+        private HeadPanning _headPanning = new HeadPanning();
+        private SkeletonData _skel;
+        private int depthHeight;
+        private int depthWidth;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            nui.Initialize(RuntimeOptions.UseColor | RuntimeOptions.UseDepth | RuntimeOptions.UseSkeletalTracking);
+            nui.VideoFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_VideoFrameReady);
+            nui.DepthFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_DepthFrameReady);
+            //nui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
+            nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
+            nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.Depth);
+            nui.NuiCamera.ElevationAngle = Camera.ElevationMaximum;
+            depthHeight = nui.DepthStream.Height;
+            depthWidth = nui.DepthStream.Width;
+            //_headPanning.Connect("127.0.0.1");
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            nui.Uninitialize();
+        }
+
+        void nui_VideoFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+            image1.Source = e.ImageFrame.ToBitmapSource();
+        }
+
+        void projectiveToRealWorld(double depth, int x_proj, int y_proj, out double x_rw, out double y_rw)
+        {
+            const double FovH = 1.0144686707507438; // (rad)
+            const double FovV = 0.78980943449644714; // (rad)
+            double XtoZ = Math.Tan(FovH / 2) * 2;
+            double YtoZ = Math.Tan(FovV / 2) * 2;
+            x_rw = (x_proj / depthWidth - .5) * depth * XtoZ;
+            y_rw = (0.5 - y_proj / depthHeight) * depth * YtoZ;
+        }
+
+        void nui_DepthFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+            // Depth is in mm
+            short[][] depth = e.ImageFrame.ToDepthArray2D();
+            image2.Source = e.ImageFrame.ToBitmapSource();
+            /*
+            if (true)
+            {
+                Matrix<double> data = new Matrix<double>(depthHeight*depthWidth, 3);
+                int row = 0;
+                for (int x=0; x < depthWidth; x++) {
+                    for (int y = 0; y < depthHeight; y++)
+                    {
+                        double depthVal = depth[x][y];
+                        depthVal /= 1000.0; // To convert into meters
+                        double x_rw, y_rw;
+                        projectiveToRealWorld(depthVal, x, y, out x_rw, out y_rw);
+                        data[row, 0] = x_rw;
+                        data[row, 1] = y_rw;
+                        data[row, 2] = depthVal;
+                    }
+                }
+                Matrix<double> evals = new Matrix<double>(1, 3);
+                Matrix<double> evecs = new Matrix<double>(3, 3);
+                Emgu.CV.CvInvoke.cvCalcPCA(data.Ptr, IntPtr.Zero, evals.Ptr, evecs.Ptr, Emgu.CV.CvEnum.PCA_TYPE.CV_PCA_DATA_AS_ROW);
+                
+                //= _skel.Joints[JointID.Head].Position;
+            }*/
+        }
+
+        void nui_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            SkeletonFrame allSkeletons = e.SkeletonFrame;
+
+            //get the first tracked skeleton
+            SkeletonData skeleton = (from s in allSkeletons.Skeletons
+                                     where s.TrackingState == SkeletonTrackingState.Tracked
+                                     select s).FirstOrDefault();
+            if (skeleton != null)
+            {
+                this._skel = skeleton;
+                Joint head = skeleton.Joints[JointID.Head];
+                
+                SetEllipsePosition(headEllipse, skeleton.Joints[JointID.Head]);
+                SetEllipsePosition(leftEllipse, skeleton.Joints[JointID.HandLeft]);
+                SetEllipsePosition(rightEllipse, skeleton.Joints[JointID.HandRight]);
+                head = head.ScaleTo(200, 200);
+                position.Text = "(" + head.Position.X + ", " + head.Position.Y + ", " + head.Position.Z + ")";
+                Console.WriteLine("(" + (head.Position.X - 100) + ", " + (head.Position.Y - 100) + ", " + (head.Position.Z - 100) + ")");
+                _headPanning.UpdatePitch(100 - (int)head.Position.Y);
+                _headPanning.UpdateYaw(100 - (int)head.Position.X);
+            }
+        }
+
+        private void SetEllipsePosition(FrameworkElement ellipse, Joint joint)
+        {
+            var scaledJoint = joint.ScaleTo(640, 480, .5f, .5f);
+
+            Canvas.SetLeft(ellipse, scaledJoint.Position.X);
+            Canvas.SetTop(ellipse, scaledJoint.Position.Y);
+        }
+    }
+}
