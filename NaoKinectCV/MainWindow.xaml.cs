06165e1a424d6e1eadd772475ae55da65eddbc97
Peter Brook <pbrook@cs.washington.edu>
2011-10-07 23:33:00 -0700

    Added NaoKinectCV project for exploring how we can use OpenCV, removed extra dependencies from the NaoKinectTest project.

---
+++ NaoKinectCV/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+using Coding4Fun.Kinect.Wpf;
+using Microsoft.Research.Kinect.Nui;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace NaoKinectCV
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Runtime nui = new Runtime();
+        private int depthHeight;
+        private int depthWidth;
+        private SkeletonData _skel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            nui.Initialize(RuntimeOptions.UseColor | RuntimeOptions.UseDepth | RuntimeOptions.UseSkeletalTracking);
+            //nui.VideoFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_VideoFrameReady);
+            nui.DepthFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_DepthFrameReady);
+            nui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
+            nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
+            nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.Depth);
+            nui.NuiCamera.ElevationAngle = Camera.ElevationMaximum;
+            depthHeight = nui.DepthStream.Height;
+            depthWidth = nui.DepthStream.Width;
+            //_headPanning.Connect("127.0.0.1");
+        }
+
+        void projectiveToRealWorld(float depth, int x_proj, int y_proj, out float x_rw, out float y_rw)
+        {
+            const float FovH = 1.0144686707507438f; // (rad)
+            const float FovV = 0.78980943449644714f; // (rad)
+            float XtoZ = (float)Math.Tan(FovH / 2) * 2;
+            float YtoZ = (float)Math.Tan(FovV / 2) * 2;
+            x_rw = (x_proj / (float)depthWidth - .5f) * depth * XtoZ;
+            y_rw = (0.5f - y_proj / (float)depthHeight) * depth * YtoZ;
+        }
+
+        void nui_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            SkeletonFrame allSkeletons = e.SkeletonFrame;
+
+            //get the first tracked skeleton
+            SkeletonData skeleton = (from s in allSkeletons.Skeletons
+                                     where s.TrackingState == SkeletonTrackingState.Tracked
+                                     select s).FirstOrDefault();
+            if (skeleton != null)
+            {
+                this._skel = skeleton;
+                Joint head = skeleton.Joints[JointID.Head];
+
+                head = head.ScaleTo(200, 200);
+                Console.WriteLine("(" + (head.Position.X - 100) + ", " + (head.Position.Y - 100) + ", " + (head.Position.Z - 100) + ")");
+                //_headPanning.UpdatePitch(100 - (int)head.Position.Y);
+                //_headPanning.UpdateYaw(100 - (int)head.Position.X);
+            }
+        }
+
+        void dumpToFile(Matrix<double> pts, string fname)
+        {
+            using (StreamWriter writer = new StreamWriter(fname))
+            {
+                for (int i = 0; i < pts.Rows; i++)
+                {
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(i);
+                    sb.Append(" ");
+                    sb.Append(pts[i, 0]);
+                    sb.Append(" ");
+                    sb.Append(pts[i, 1]);
+                    sb.Append(" ");
+                    sb.Append(pts[i, 2]);
+                    sb.Append(" ");
+                    sb.Append(1); // Group ID
+                    writer.WriteLine(sb.ToString());
+                }
+            }
+        }
+
+        void nui_DepthFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+            // Depth is in mm
+            short[][] depth = e.ImageFrame.ToDepthArray2D();
+            //image2.Source = e.ImageFrame.ToBitmapSource();
+            if (this._skel != null)
+            {
+                Matrix<double> data = new Matrix<double>(depthHeight * depthWidth, 3);
+                List<MCvPoint3D32f> head_points = new List<MCvPoint3D32f>();
+                MCvPoint3D32f head_pos = new MCvPoint3D32f();
+                head_pos.x = this._skel.Joints[JointID.Head].Position.X;
+                head_pos.y = this._skel.Joints[JointID.Head].Position.Y;
+                head_pos.z = this._skel.Joints[JointID.Head].Position.Z;
+                
+                int row = 0;
+                for (int x = 0; x < depthWidth; x++)
+                {
+                    for (int y = 0; y < depthHeight; y++)
+                    {
+                        float depthVal = depth[x][y];
+                        depthVal /= 1000.0f; // To convert into meters
+                        float x_rw, y_rw;
+                        projectiveToRealWorld(depthVal, x, y, out x_rw, out y_rw);
+
+                        MCvPoint3D32f pt = new MCvPoint3D32f(x_rw, y_rw, depthVal);
+                        MCvPoint3D32f delta = pt - head_pos;
+
+                        if (delta.Norm < 0.3) // If the point is closer than 10cm
+                        {
+                            head_points.Add(pt);
+                        }
+
+                        data[row, 0] = x_rw;
+                        data[row, 1] = y_rw;
+                        data[row, 2] = depthVal;
+                        row++;
+                    }
+                }
+
+                // Get a matrix of all of the points in the head
+                Matrix<double> head_pts = new Matrix<double>(head_points.Count, 3);
+                for (int i=0; i < head_points.Count; i++)
+                {
+                    MCvPoint3D32f p = head_points[i];
+                    head_pts[i, 0] = p.x;
+                    head_pts[i, 1] = p.y;
+                    head_pts[i, 2] = p.z;
+                }
+                dumpToFile(head_pts, "head");
+                dumpToFile(data, "scene");
+                if (head_points.Count > 0)
+                {
+                    Matrix<double> evals = new Matrix<double>(1, 3);
+                    Matrix<double> avg = new Matrix<double>(1, 3);
+                    Matrix<double> evecs = new Matrix<double>(3, 3);
+                    Emgu.CV.CvInvoke.cvCalcPCA(head_pts.Ptr, avg.Ptr, evals.Ptr, evecs.Ptr, Emgu.CV.CvEnum.PCA_TYPE.CV_PCA_DATA_AS_ROW);
+                    Console.WriteLine("<" + evals[0, 0] + ", " + evals[0, 1] + ", " + evals[0, 2] + ">");
+                }
+                //= _skel.Joints[JointID.Head].Position;
+            }
+        }
+    }
+}
--- NaoKinectTest/MainWindow.xaml.cs
+++ NaoKinectTest/MainWindow.xaml.cs
@@ -6,9 +6,6 @@ using Aldebaran.NaoCamCSharpExample;
 using Coding4Fun.Kinect.Wpf;
 using Microsoft.Research.Kinect.Nui;
 
-using Emgu.CV;
-using Emgu.CV.Structure;
-
 namespace NaoKinectTest
 {
     /// <summary>
@@ -32,13 +29,13 @@ namespace NaoKinectTest
             nui.Initialize(RuntimeOptions.UseColor | RuntimeOptions.UseDepth | RuntimeOptions.UseSkeletalTracking);
             nui.VideoFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_VideoFrameReady);
             nui.DepthFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_DepthFrameReady);
-            //nui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
+            nui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
             nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
             nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.Depth);
             nui.NuiCamera.ElevationAngle = Camera.ElevationMaximum;
             depthHeight = nui.DepthStream.Height;
             depthWidth = nui.DepthStream.Width;
-            //_headPanning.Connect("127.0.0.1");
+            _headPanning.Connect("127.0.0.1");
         }
 
         private void Window_Closed(object sender, EventArgs e)
@@ -51,44 +48,10 @@ namespace NaoKinectTest
             image1.Source = e.ImageFrame.ToBitmapSource();
         }
 
-        void projectiveToRealWorld(double depth, int x_proj, int y_proj, out double x_rw, out double y_rw)
-        {
-            const double FovH = 1.0144686707507438; // (rad)
-            const double FovV = 0.78980943449644714; // (rad)
-            double XtoZ = Math.Tan(FovH / 2) * 2;
-            double YtoZ = Math.Tan(FovV / 2) * 2;
-            x_rw = (x_proj / depthWidth - .5) * depth * XtoZ;
-            y_rw = (0.5 - y_proj / depthHeight) * depth * YtoZ;
-        }
-
         void nui_DepthFrameReady(object sender, ImageFrameReadyEventArgs e)
         {
             // Depth is in mm
-            short[][] depth = e.ImageFrame.ToDepthArray2D();
             image2.Source = e.ImageFrame.ToBitmapSource();
-            /*
-            if (true)
-            {
-                Matrix<double> data = new Matrix<double>(depthHeight*depthWidth, 3);
-                int row = 0;
-                for (int x=0; x < depthWidth; x++) {
-                    for (int y = 0; y < depthHeight; y++)
-                    {
-                        double depthVal = depth[x][y];
-                        depthVal /= 1000.0; // To convert into meters
-                        double x_rw, y_rw;
-                        projectiveToRealWorld(depthVal, x, y, out x_rw, out y_rw);
-                        data[row, 0] = x_rw;
-                        data[row, 1] = y_rw;
-                        data[row, 2] = depthVal;
-                    }
-                }
-                Matrix<double> evals = new Matrix<double>(1, 3);
-                Matrix<double> evecs = new Matrix<double>(3, 3);
-                Emgu.CV.CvInvoke.cvCalcPCA(data.Ptr, IntPtr.Zero, evals.Ptr, evecs.Ptr, Emgu.CV.CvEnum.PCA_TYPE.CV_PCA_DATA_AS_ROW);
-                
-                //= _skel.Joints[JointID.Head].Position;
-            }*/
         }
 
         void nui_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
@@ -103,7 +66,7 @@ namespace NaoKinectTest
             {
                 this._skel = skeleton;
                 Joint head = skeleton.Joints[JointID.Head];
-                
+
                 SetEllipsePosition(headEllipse, skeleton.Joints[JointID.Head]);
                 SetEllipsePosition(leftEllipse, skeleton.Joints[JointID.HandLeft]);
                 SetEllipsePosition(rightEllipse, skeleton.Joints[JointID.HandRight]);
