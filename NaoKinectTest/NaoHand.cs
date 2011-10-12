aecead653d72267eba87c9ddad0672c59824265f
Ryan Oman <omanamos@gmail.com>
2011-10-11 18:34:50 -0700

    finished functionality on individual joints for the NaoSeleton

--- NaoKinectTest/NaoHand.cs
+++ NaoKinectTest/NaoHand.cs
@@ -7,5 +7,18 @@ namespace NaoKinectTest
 {
     class NaoHand
     {
+        private bool opened;
+
+        private NaoHand() { }
+
+        public NaoHand(bool opened)
+        {
+            this.opened = opened;
+        }
+
+        public bool isOpen()
+        {
+            return this.opened;
+        }
     }
 }
--- NaoKinectTest/NaoWrist.cs
+++ NaoKinectTest/NaoWrist.cs
@@ -7,5 +7,27 @@ namespace NaoKinectTest
 {
     class NaoWrist
     {
+        public static sealed double YAW_MIN = -1.8238;
+        public static sealed double YAW_MAX = 1.8238;
+        public static sealed double YAW_RANGE = YAW_MAX - YAW_MIN;
+
+        private double yaw;
+
+        private NaoWrist(){}
+
+        public NaoWrist(double humanYaw)
+        {
+            this.yaw = scaleYaw(humanYaw);
+        }
+
+        public double getYaw()
+        {
+            return this.getYaw();
+        }
+
+        private double scaleYaw(double humanYaw)
+        {
+            return (humanYaw - HumanShoulder.YAW_MIN / HumanShoulder.YAW_RANGE) * YAW_RANGE;
+        }
     }
 }
