7ac91d7f9e56551b0270b47a153f713a77074efb
Peter Brook <pbrook@cs.washington.edu>
2011-11-02 22:25:50 -0700

    added missing util

---
+++ Recognizer/Util.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recognizer
+{
+    class Util
+    {
+        public static bool shouldUseJVals(string actName)
+        {
+            string[] jointActs = {"wave right", "wave left", "raise the roof", "macarena"};
+            string[] posActs = {"walk forward", "walk backward", "walk left", "walk right"};
+            
+            bool useJoint = false;
+            foreach (string s in jointActs)
+            {
+                if (actName.IndexOf(s) > -1)
+                    useJoint = true;
+            }
+            return useJoint;
+        }
+    }
+}
