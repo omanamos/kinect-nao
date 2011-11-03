e42978025529ccbc7c5123f47a5167d75a1b97fd
Peter Brook <pbrook@cs.washington.edu>
2011-11-03 00:20:04 -0700

    added program for going from recorded human data to executing action on NAO

---
+++ NaoPlayer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using DataStore;
+
+namespace NaoPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "Z:/WindowsFolders/Desktop/waveleft/wave left1.rec";
+
+            List<HumanSkeleton> seq = new List<HumanSkeleton>();
+            using (StreamReader s = new StreamReader(filename))
+            {
+                while (!s.EndOfStream)
+                {
+                    seq.Add(new HumanSkeleton(s.ReadLine()));
+                }
+            }
+
+            List<NaoSkeleton> naoSeq = new List<NaoSkeleton>();
+            foreach (HumanSkeleton h in seq)
+            {
+                AngleConverter ac = new AngleConverter(h);
+                NaoSkeleton ns = ac.getNaoSkeleton();
+                naoSeq.Add(ns);
+            }
+            ActionSequence<NaoSkeleton> naoActs = new ActionSequence<NaoSkeleton>(naoSeq);
+            bool isJointAction = true; // false if is a walking action
+            ExecuteOnNao(naoActs, isJointAction);
+        }
+
+        static void ExecuteOnNao(ActionSequence<NaoSkeleton> actSeq, bool isJointAction)
+        {
+            // do stuff with naoskeletons and real NAO
+        }
+
+    }
+}
